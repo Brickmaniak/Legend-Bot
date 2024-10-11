45530651bbdd56e3c90194231dc62e197d6b55f8
Brickmaniak <diego.milliand@proton.me>
2024-10-11 07:30:17 +0200

    Update main.py
    Bugfix:
    I had forgotten to add "import datetime" to be able to use antiraid correctly 😅

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from discord import app_commands
 from discord.ext import commands,tasks
 import random
 import json
+import datetime
 
 intents = discord.Intents().all()
 bot = commands.Bot(command_prefix = "+", intents=intents)
